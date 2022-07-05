9c484da315dfe365d8c303f48cb1e6a415e7d393
Rafacribeiro <rafaelacosta.ep@gmail.com>
2022-07-05 11:41:02 -0300

    segundo commit

--- data-fetching/src/main.tsx
+++ data-fetching/src/main.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import {App} from './App'
+import { QueryClientProvider } from "react-query"
+import { queryClient } from './services/queryClient'
+import { BrowserRouter} from "react-router-dom"
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
+    </BrowserRouter>
   </React.StrictMode>
 )
